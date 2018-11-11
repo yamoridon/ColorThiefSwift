0ed795f01bbc1b6ea94698a6e433caace4e73cbf
Kazuki Ohara <kazuki.ohara@gmail.com>
2018-11-12 02:11:41 +0900

    remove files for Travis.

--- run_test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-xcodebuild test -workspace Example/ColorThiefSwift.xcworkspace -scheme ColorThiefSwift-Example -sdk iphonesimulator10.2 -destination 'platform=iOS Simulator,name=iPhone 7,OS=10.2' ONLY_ACTIVE_ARCH=NO
